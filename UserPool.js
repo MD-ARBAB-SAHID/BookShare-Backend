58cc48478421c9ca2030dc8f2906c866938f1c38
MD-ARBAB-SAHID <arbabsahid7@gmail.com>
2022-12-08 01:50:31 +0530

    changes to env variables

--- UserPool.js
+++ UserPool.js
@@ -1,8 +1,8 @@
-const { CognitoUserPool } = require('amazon-cognito-identity-js');
+const { CognitoUserPool } = require("amazon-cognito-identity-js");
 require("dotenv").config();
 const poolData = {
-    UserPoolId: process.env.UserPoolId,
-    ClientId: process.env.ClientId
-}
+  UserPoolId: process.env.UserPoolId,
+  ClientId: process.env.AWS_ClientId,
+};
 
 module.exports = new CognitoUserPool(poolData);
